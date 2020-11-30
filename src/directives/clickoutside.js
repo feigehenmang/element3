5b7e021e77343b2cbf09d24b52f58d5fdbc4bef1
lijinfei <1638546595@qq.com>
2020-11-30 22:39:28 +0800

    clickoutside 解析

--- src/directives/clickoutside.js
+++ src/directives/clickoutside.js
@@ -8,12 +8,13 @@ let seed = 0
 
 // !Vue.prototype.$isServer && on(document, 'mousedown', e => (startClick = e))
 on(document, 'mousedown', (e) => (startClick = e))
-
+// mousedown 和 mouseup 可以监控到点击事件 以及 事件target不同意的情况
+// 在页面初始化时就会将事件注册 只注册一次  不会重复注册 在 angular中运行的地方可能就是constructor
 on(document, 'mouseup', (e) => {
   // !Vue.prototype.$isServer && on(document, 'mouseup', e => {
   nodeList.forEach((node) => node[ctx].documentHandler(e, startClick))
 })
-
+// 创建函数 在指令初始化之前就将要执行的函数 以及 当前elment节点存储 在documentonclick的时候执行
 function createDocumentHandler(el, binding, vnode) {
   return function (mouseup = {}, mousedown = {}) {
     if (
